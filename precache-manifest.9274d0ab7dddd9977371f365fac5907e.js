5d264e3991bd40c8acc138ac50fa96d385f7e912
lukasmpower <53568353+lukasmpower@users.noreply.github.com>
2020-03-31 16:20:54 +0100

    Updates

---
+++ precache-manifest.9274d0ab7dddd9977371f365fac5907e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fe73ba354ff99c5a8fd6bec039aa78",
+    "url": "/ctracker/index.html"
+  },
+  {
+    "revision": "335ef4f12d0d2b7c2b9d",
+    "url": "/ctracker/static/css/main.7e1503b2.chunk.css"
+  },
+  {
+    "revision": "ff64d24c1fb5cddc1c80",
+    "url": "/ctracker/static/js/2.d1bdee47.chunk.js"
+  },
+  {
+    "revision": "f8a8b806455b53cbdd585b03974fb114",
+    "url": "/ctracker/static/js/2.d1bdee47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335ef4f12d0d2b7c2b9d",
+    "url": "/ctracker/static/js/main.1211f60e.chunk.js"
+  },
+  {
+    "revision": "4d70435e9908e9690fca",
+    "url": "/ctracker/static/js/runtime-main.0fdfd3be.js"
+  }
+]);
